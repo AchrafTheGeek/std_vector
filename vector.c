706c34c2670f110afba833d2388d2ebab0451c3c
Achraf Khadraoui <75825038+AchrafTheGeek@users.noreply.github.com>
2025-11-29 02:02:17 +0100

    added: capacity check and list the vector value
    implemented a list function to list all values inside a vector
    implemented a capacity check function to make sure the vector has enough capacity.

--- vector.c
+++ vector.c
@@ -18,12 +18,79 @@ void vector_init(vector *v) {
 	}
 }
 
-void vector_push(vector *v) {
+int vector_capacity_check(vector *v) {
+	if (v->size < v->capacity) {
+		return 0;
+	}
+
+	size_t new_cap = v->capacity * 2;
+
+	int *temp_data = realloc(v->data, v->capacity * sizeof(int));
+	if (!temp_data) {
+		perror("Error: Could not reallocate memory for the vector.\n");
+		return -1;
+	}
+
+	v->data = temp_data;
+	v->capacity = temp_cap;
+	return 0;
+}
+
+void list_vector(vector *v) {
+	if (!v->size) {
+		printf("Info: The vector is empty.\n");
+		return;
+	}
+
+	for (int i = 0; i < v->size; ++i) {
+		printf("%d\n", v->data[i]);
+	}
+}
+
+void vector_push(vector *v, int value) {
+	if (vector_capacity_check(v)) {
+		perror("Error: Could not push the value to the vector");
+		return;
+	}
 	
+	v->data[size] = value;
 }
 
 
 int main(void) {
+	vector *v;
+	vector_init(v);
+	
+	int choice;
+
+
+	do {
+		printf("\n--- std vector in C ---\n");
+		printf("1- List the values inside the vector.\n");
+
+		if (scanf("%d", &choice) != 1) {
+			printf("Error: Invalid inpout. Please enter a valid choice.\n");
+			continue;
+		}
+	
+		switch (choice) {
+			case 1:
+				list_vector(v);
+				break;
+			case 2:
+				vector_push(v);
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				printf("Exiting application. Have a nice day.\n");
+				exit(0);
+			default:
+				printf("Error: Unkown command. Please enter a valid choice.\n");
+				break;
+		}
+	} while(choice != 6);
+
 
 	return 0;
 }
